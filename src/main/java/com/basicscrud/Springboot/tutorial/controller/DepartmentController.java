0f7087887a1f91dcc68adb95ff9d0a571285f173
maruf-swe <marufur1847@gmail.com>
2023-12-20 01:04:10 +0600

    Add Logger class

--- src/main/java/com/basicscrud/Springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/com/basicscrud/Springboot/tutorial/controller/DepartmentController.java
@@ -4,6 +4,8 @@ package com.basicscrud.Springboot.tutorial.controller;
 import com.basicscrud.Springboot.tutorial.entity.Department;
 import com.basicscrud.Springboot.tutorial.service.DepartmentService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +17,22 @@ public class DepartmentController {
 
     private final DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
     }
 
     @PostMapping("/save")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside the SaveDepartment Controller");
         return departmentService.saveDepartment(department);
 
     }
 
     @GetMapping("/departments")
     public List<Department> getDepartmentList() {
+        LOGGER.info("Inside the getDepartmentList Controller");
         return departmentService.getDeparmentList();
     }
 
